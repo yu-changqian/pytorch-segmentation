2b03ae78675be869fbf0881f3990f918e19f17bf
ycszen <changqian_yu@163.com>
2017-03-16 21:16:38 +0800

    a litte fix

--- trainer.py
+++ trainer.py
@@ -7,11 +7,11 @@ from loss import CrossEntropy2d
 
 
 trainloader = data.DataLoader(VOCDataSet("./data", is_transform=True), batch_size=4,
-                                num_workers=8)
+                                num_workers=8, pin_memory=True)
 
 model = ResNet50()
 if torch.cuda.is_available():
-    model.cuda()
+    model.cuda(0)
 
 lr = 0.001
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
@@ -19,22 +19,22 @@ optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 for epoch in range(80):
     for i, (images, labels) in enumerate(trainloader):
         if torch.cuda.is_available():
-            images = Variable(images.cuda())
-            labels = Variable(images.cuda())
+            images = Variable(images.cuda(0))
+            labels = Variable(images.cuda(0))
         else:
             images = Variable(images)
             labels = Variable(labels)
 
         optimizer.zero_grad()
         outputs = model(images)
-        loss = CrossEntropy2d(outputs, labels, size_average=False)
+        loss = CrossEntropy2d(outputs, labels, size_average=True)
         loss /= len(images)
         loss.backward()
         optimizer.step()
 
-        if (i+1) % 100 == 0:
+        if (i+1) % 20 == 0:
             print("Epoch [%d/%d] Iter [%d/%d] Loss: %.4f" % (epoch+1, 80,
-                    i+1, 2000, loss.data[0]))
+                    i+1, 3000, loss.data[0]))
 
     if (epoch+1) % 20 == 0:
         lr /= 3
