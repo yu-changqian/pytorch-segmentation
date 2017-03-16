b1a6c5c3c31e9af103d331a27efc0e9a962d1930
ycszen <changqian_yu@163.com>
2017-03-16 17:15:17 +0800

    add conv_transpose

--- resnet.py
+++ resnet.py
@@ -12,10 +12,10 @@ class Bottleneck(nn.Module):
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3,
                                 stride=stride, bias=False, padding=1)
         self.bn2 = nn.BatchNorm2d(out_channels)
-        self.conv3 = nn.Conv2d(out_channels, out_channels*expansion,
+        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion,
                                 kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNOrm2d(out_channels*expansion)
-        self.relu = nn.ReLU(inplace=True)
+        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
+        self.relu = nn.ReLU()
         self.downsample = downsample
 
     def forward(self, x):
@@ -42,23 +42,23 @@ class Bottleneck(nn.Module):
         return out
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_layers):
-        super(ResNet, self).__init()
+    def __init__(self, block, num_layers, n_classes):
+        super(ResNet, self).__init__()
 
         self.in_channels = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
                                 bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
         self.layer1 = self._make_layer(block, 64, num_layers[0])
         self.layer2 = self._make_layer(block, 128, num_layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_layers[3], stride=2)
-        self.upsample1 = nn.ConvTranspose2d(2048, 512, kernel_size=11)
-        self.upsample2 = nn.ConvTranspose2d
+        self.conv1_1 = nn.Conv2d(2048, n_classes, kernel_size=1, stride=1, bias=False)
+        self.upsample = nn.ConvTranspose2d(n_classes, n_classes, kernel_size=32, stride=32)
 
     def _make_layer(self, block, init_channels, num_layer, stride=1):
         downsample = None
@@ -77,6 +77,7 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        img = x
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
@@ -86,6 +87,14 @@ class ResNet(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-        x = self.upsample(x)
+        x = self.conv1_1(x)
+        x = self.upsample(x, output_size=img.size())
 
         return x
+
+
+def ResNet50(**kwargs):
+    return ResNet(Bottleneck, [3, 4, 6, 3], 21, **kwargs)
+
+def ResNet101(**kwargs):
+    return ResNet(Bottleneck, [3, 4, 23, 3], 21, **kwargs)
